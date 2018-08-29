89457e96f19e414b68656782756634bc6e8d69fa
Thomas Marquis <thomas.marquis.dev@gmail.com>
2018-08-29 12:29:08 +0200

    QGridLayout

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
-from PySide2.QtWidgets import QApplication, QPushButton, QWidget
+from PySide2.QtWidgets import QApplication, QPushButton, QWidget, QGridLayout
 
 
 class MyApplication(QWidget):
     def __init__(self):
         super().__init__()
 
-        btn_hello = QPushButton('Hello !', self)
-        btn_bye = QPushButton('Bye !', self)
+        layout = QGridLayout(self)
+
+        button = QPushButton('grand bouton')
+        layout.addWidget(button, 0, 0, 1, 3)
+
+        for i in range(1, 3):
+            for j in range(0, 3):
+                button = QPushButton('{}, {}'.format(i, j))
+                layout.addWidget(button, i, j, 1, 1)
 
         self.resize(500, 500)
 
