d5d6828154d7628913d9b3101c4e4c5394dcd257
Thomas Marquis <thomas.marquis.dev@gmail.com>
2018-09-21 14:02:44 +0200

    setup connections

--- notesManager_ui.py
+++ notesManager_ui.py
@@ -9,7 +9,29 @@ class NoteManager(QtWidgets.QWidget, Ui_NotesManager):
         super(NoteManager, self).__init__()
 
         self.setupUi(self)
+        self._setup_connections()
         self.show()
+
+    def _setup_connections(self):
+        self.create_note_btn.clicked.connect(self._create_note)
+        self.notes_list_output.itemClicked.connect(self._display_note)
+        self.update_note_btn.clicked.connect(self._update_note)
+        self.delete_note_btn.clicked.connect(self._delete_note)
+
+    def _create_note(self):
+        print('créer une note')
+
+    def _display_note(self):
+        print('afficher la note')
+
+    def _update_note(self):
+        print('mettre à jour la note')
+
+    def _delete_note(self):
+        print('supprimer la note')
+
+    def _get_notes(self):
+        pass
 
 
 app = QtWidgets.QApplication([])
